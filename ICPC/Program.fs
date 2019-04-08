1288afa3bb64dc8fffe41965bd9af18aa3639cb9
redy <redy39@gmail.com>
2019-04-08 12:55:29 +0200

    me again

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -32,22 +32,31 @@ let commaSprinkler (input:string) : string option =
   |Some a -> a
   |None -> []
 
- let rec wordafter (lst:char list) (idx:int) (lst2:char list) : char list option= ////returns the word(as a char list) after the the seplyed index
+ let rec wordafter (lst:char list) (idx:int) (lst2:char list) : char list option= ////returns the word(as a char list) after the the seplyed index (index of comma +1)
   match idx with 
   | -1 -> None
   | _ -> match (lst.[idx]=lst.[(List.length (lst))-1]) with
-         |true -> Some lst2     //at the end of the sentance
+         |true -> Some lst2     //at the end of the sentance 
          |false -> match lst.[idx+1] with 
-                   |' ' -> wordafter lst (idx+1) lst2
+                   |' ' -> Some lst2
                    |'.' -> Some lst2
                    |a -> wordafter lst (idx+1) (lst2@[a])
  
  let Aword = 
-  match wordafter inList idx [] with
+  match wordafter inList (idx+1) [] with
   |Some a -> a
   |None -> []
 
- None
+ let rec matchwordbefore (lst:char list) (word:char list) (idx:int) (widx:int) (count:int): int option= //returns the starting index(int option) of the first occurance of a matching word after a serplyed index
+  match (lst.[idx]=word.[(List.length word)-1]) with
+  |true -> Some (idx-count)      //at the end of the word
+  |false -> match (lst.[idx] = word.[widx]) with
+            |true -> matchwordbefore lst word (idx+1) (widx+1) (count+1)
+            |false -> matchwordbefore lst word (idx+1) widx count 
+ 
+ let idxwordbefore = matchwordbefore inList Bword 0 0 0
+ 
+
 let rivers input =
     failwith "Not implemented"
 
