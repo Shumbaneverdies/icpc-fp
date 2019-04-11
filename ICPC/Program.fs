384ddeeaf4baa92c1c2dfc2aad297a7c034eff8b
redy <redy39@gmail.com>
2019-04-11 12:21:26 +0200

    re-wrote my code

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -2,76 +2,64 @@
 open System
 
 let commaSprinkler (input:string) : string option = 
- match (String.length input) < 2 with
- |true -> None //invalid string
- |false -> match (input.[0]=' ',input.[0]=',',not (input.[(String.length input)-1]='.')) with 
+ let valid1 (str:string) : string option = //checks the input for validiy
+  match (str|>String.length) < 2 with
+  |true -> None //input too short
+  |false -> match (str.[0]=' ',str.[0]=',',not (str.[(str|>String.length)-1]='.')) with 
            |true,_,_ |_,true,_ |_,_,true -> None //invalid string
-           |_ -> 
-   let inList = List.ofSeq input in  
+           |_ -> Some str
 
-   let rec valid (lst:char list) = 
-    match lst.[0]=lst.[(List.length lst)-1] with 
-    |true -> "valid"
-    |false -> match (not (lst.[0]=' '),not (lst.[0]=','),not (lst.[0]='.'),not (Char.IsLower lst.[0])) with
-              |true,_,_,_ |_,true,_,_ |_,_,true,_ |_,_,_,true -> "not valid"
-              |_ -> valid lst.[1..]
-    
-    match valid inList with 
-    |"not valid" -> None
-    |_ -> 
-    let rec findcomma (lst:char list) (idx:int) : int option = //returns the index(int option) of the first occurence of a comma(after a given index) else returns None
-     match lst.[idx]=lst.[(List.length (lst))-1] with
-     |true -> None       //if there are no commas
-     |false -> match lst.[idx] with 
-               |',' -> Some idx
-               |_ -> findcomma inList (idx+1)
-  
-    match findcomma inList 0 with 
-    |None -> Some input
-    |Some a -> let idx = a 
-              
-               match inList.[idx-1]=' ',not (inList.[idx+1]=' ') with
-               |true,_ |_,true -> None
-               |_ -> 
+ let rec valid2 (str:string option) (idx:int): string option = //checks that all chars are valid
+  match str with
+  |None -> None
+  |Some a -> match a.[idx]=a.[(a|>String.length)-1] with 
+             |true -> str
+             |false -> match (a.[idx]=' ',a.[idx]=',',a.[idx]='.',Char.IsLower a.[idx]) with
+                       |true,_,_,_ |_,true,_,_ |_,_,true,_ |_,_,_,true -> valid2 str (idx+1)
+                       |_ -> None
+
+ let rec findcomma (lst:char list) (idx:int) : int option = //returns the index(int option) of the first occurence of a comma(after a given index) else returns None
+  match lst.[idx]=lst.[(List.length (lst))-1] with
+  |true -> None       //if there are no commas
+  |false -> match lst.[idx] with 
+            |',' -> Some idx
+            |_ -> findcomma lst (idx+1)
+
+ let rec findperiod (lst:char list) (idx:int) : int option = //returns the index(int option) of the first occurence of a period(after a given index) else returns None
+  match lst.[idx] with 
+  |'.' -> Some idx
+  |_ -> findperiod lst (idx+1)
  
-               let rec wordbefore (lst:char List) (idx:int) (lst2:char List) : char List option= //returns the word(as a char list) before the the seplyed index
-                match (lst.[idx] = lst.[0]) with 
-                |true -> Some lst2         //at the begining of the sentance
-                |false -> match lst.[idx-1] with 
-                          |' ' -> Some lst2
-                          |a -> wordbefore lst (idx-1) (a::lst2)
-  
-               let Bword = 
-                match wordbefore inList idx [] with 
-                |Some a -> a
-                |None -> []
+ let rec valid3 (str:string option) idx : string option = //checks that there is a space after each comma, no space before a comma and no space too positions after a comma
+  match str with 
+  |None -> None
+  |Some a -> let list = a|>List.ofSeq
+             match (findcomma list idx) with
+             |None -> str                                                         //no comma thus i am done! 
+             |Some CIDX -> match a.[CIDX-1]=' ',a.[CIDX+1]=' ',a.[CIDX+2]=' ' with    //CIDX is the index of the comma
+                           |true,_,_ |_,false,_ |_,_,true -> None
+                           |_ -> valid3 str (CIDX+1)
 
-               let rec wordafter (lst:char list) (idx:int) (lst2:char list) : char list option= ////returns the word(as a char list) after the the seplyed index (index of comma +1)
-                match (lst.[idx]=lst.[(List.length (lst))-1]) with
-                |true -> Some lst2     //at the end of the sentance 
-                |false -> match lst.[idx+1] with 
-                          |' ' -> Some lst2
-                          |'.' -> Some lst2
-                          |a -> wordafter lst (idx+1) (lst2@[a])
+ let rec valid4 (str:string option) (idx:int) : string option = //simular to valid3 but with periods
+  match str with 
+  |None -> None
+  |Some a -> let list = a|>List.ofSeq
+             match (findperiod list idx) with 
+             |None -> None //should never reach this point
+             |Some PIDX -> match PIDX = ((List.length (list))-1) with                                      //PIDX is the index of the period
+                           |true -> str 
+                           |false -> match a.[PIDX-1]=' ',a.[PIDX+1]='.',a.[PIDX+2]='.',a.[PIDX+2]=' ' with
+                                     |true,_,_,_ |_,false,_,_ |_,_,true,_ |_,_,_,true -> None
+                                     |_ -> valid4 str (PIDX+1)
+
+
+
+ let input = valid1 input
+ let input = valid2 input 0
+ let input = valid3 input 0
+ let input = valid4 input 0 
  
-               let Aword = 
-                match wordafter inList (idx) [] with
-                |Some a -> a
-                |None -> []
-               
-               let rec matchwordbefore (lst:char list) (word:char list) (idx:int) (widx:int) (count:int): int option= //returns the starting index(int option) of the first occurance of a matching word after a serplyed index
-                match (lst.[idx]=word.[(List.length word)-1]) with
-                |true -> Some (idx-count)      //at the end of the word
-                |false -> match (lst.[idx] = word.[widx]) with
-                          |true -> matchwordbefore lst word (idx+1) (widx+1) (count+1)
-                          |false -> matchwordbefore lst word (idx+1) widx count 
-  
- 
-               let idxwordbefore = matchwordbefore inList Bword 0 0 0
-
-
-               Some "pie"
-
+ input
 let rivers input =
     failwith "Not implemented"
 
